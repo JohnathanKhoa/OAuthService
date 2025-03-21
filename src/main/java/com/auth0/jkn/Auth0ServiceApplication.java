314b8e649fc70bc83dd79a76585e75a3754f68d1
JohnathanKhoa <johnathan.k.nguyen.95@gmail.com>
2025-03-21 07:28:06 -0700

    refactor

--- src/main/java/com/auth0/jkn/Auth0ServiceApplication.java
+++ src/main/java/com/auth0/jkn/Auth0ServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class Auth0ServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(Auth0ServiceApplication.class, args);
 	}
 
 }
